e28bfef46d4b68b8b1bc6240a26aed412a262b78
Holly Young <young.holly.216@gmail.com>
2025-12-19 19:57:45 -0500

    Updated Main File

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from create_nodes import open_images
 from create_nodes import parse_metadata
 from create_graph import create_graph
+import cv2
 
 def main():
-    nodes = parse_metadata()
+    images = open_images
+    nodes = parse_metadata(images)
     graph = create_graph()
     graph.build_graph(nodes)
